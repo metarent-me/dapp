f914db93ab1567ebeee4a5a9e85e0585d43cd911
Shawn Tian <hightian@gmail.com>
2021-12-02 05:05:28 +0800

    add lend dialog

--- src/views/lend/Lend.vue
+++ src/views/lend/Lend.vue
@@ -4,9 +4,45 @@
     <div class="lend-nfts">
       <div class="lend-nfts-item" v-for="nft of nfts" :key="nft.id">
         <NFT nft="nft" />
-        <el-button type="primary">Lend</el-button>
+        <el-button type="primary" @click="dialogVisible = true">Lend</el-button>
       </div>
     </div>
+
+    <el-dialog
+      class="lend-dialog"
+      title="Complete checkout"
+      :visible.sync="dialogVisible"
+      width="60%"
+      :before-close="handleClose"
+      center
+    >
+      <div class="lend-dialog-desc">
+        <div class="lend-desc-collection">The Sandbox</div>
+        <div class="lend-desc-name">The Sandbox #321</div>
+      </div>
+
+      <div class="lend-dialog-form">
+        <el-form label-position="right" label-width="200px" :model="lendInfo">
+          <el-form-item label="Collateral">
+            <el-input v-model="lendInfo.collateral"></el-input>
+          </el-form-item>
+          <el-form-item label="Daily price">
+            <el-input v-model="lendInfo.dailyPrice"></el-input>
+          </el-form-item>
+          <el-form-item label="Max duration">
+            <el-input v-model="lendInfo.maxDuration"></el-input>
+          </el-form-item>
+          <el-form-item label="Fee">2.5%</el-form-item>
+        </el-form>
+      </div>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="dialogVisible = false"
+          >Approve</el-button
+        >
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -19,6 +55,12 @@ export default {
   components: { Categoryfilter, NFT },
   data() {
     return {
+      dialogVisible: false,
+      lendInfo: {
+        collateral: null,
+        dailyPrice: null,
+        maxDuration: null,
+      },
       nfts: [
         { id: 1 },
         { id: 2 },
@@ -70,5 +112,29 @@ export default {
     width: 100%;
     margin: 0 auto;
   }
+}
+
+.lend-desc-collection {
+  font-weight: bold;
+  font-size: 20px;
+}
+
+.lend-dialog {
+  .el-dialog__header {
+  }
+
+  .el-dialog__title {
+    font-weight: bold;
+    font-size: 25px;
+  }
+}
+
+.lend-dialog-desc {
+  text-align: center;
+}
+
+.lend-dialog-form {
+  width: 80%;
+  margin-top: 20px;
 }
 </style>
