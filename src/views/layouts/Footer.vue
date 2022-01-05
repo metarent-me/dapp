4797acf2fe3c45d2b4dcf95bc726f0610c0a431d
Shawn Tian <hightian@gmail.com>
2022-01-05 22:33:08 +0800

    update footer

--- src/views/layouts/Footer.vue
+++ src/views/layouts/Footer.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="footer-wrapper">
-    <div class="footer-rights">© 2021 - 2021 Metarent, Inc</div>
+    <div class="footer-rights">© 2021 - 2022 Metarent, Inc</div>
     <div class="footer-links">
-      <div>Contracts</div>
-      <div>Discord</div>
-      <div>Twitter</div>
+      <div>
+        <el-link
+          href="https://github.com/Extend-Labs/metarent-frontend"
+          target="_blank"
+          >Contracts</el-link
+        >
+      </div>
+      <div>
+        <el-link href="https://discord.gg/nW5e2QZ2aC" target="_blank"
+          >Discord</el-link
+        >
+      </div>
+      <div>
+        <el-link href="https://twitter.com/metarent_me" target="_blank"
+          >Twitter</el-link
+        >
+      </div>
     </div>
   </div>
 </template>
